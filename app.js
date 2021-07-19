d14cb89fab697422f7fd9658fa21153e57dde9c4
hrushi-thorat <hrushikeshst54@gmail.com>
2021-07-19 03:35:16 -0700

    final

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ const todoInput=document.querySelector(".input")
 const todobutton=document.querySelector(".button")
 const todolist=document.querySelector(".list")
 
-
+// add event listner
 
 todobutton.addEventListener("click", addList);
 todolist.addEventListener("click",deleteCheck);
 
-
-
+// function to create event listner
+// add div 
 function addList(e){
 e.preventDefault();
 const toDoDiv=document.createElement('div');
 toDoDiv.classList.add('todo');
-
+// add list li
 const item=document.createElement("li")
 item.innerText=todoInput.value;
 item.classList.add("list-style")
 toDoDiv.appendChild(item);
-
+// add check button
 const checked=document.createElement("button");
 checked.innerHTML="<i class='bx bx-check-circle' ></i>"
 checked.classList.add("check-btn");
 toDoDiv.appendChild(checked);
-
+// add delete button
 const remove=document.createElement("button");
 remove.innerHTML="<i class='bx bx-trash' ></i>"
 remove.classList.add("remove-btn");
